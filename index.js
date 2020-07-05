a34b295477a50ec7403e6b2c5de6171f1b29f6a6
github username <github email address>
2020-07-05 12:16:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(arr, func) {
+    let new_arr = []
+    arr.forEach(element => {
+        new_arr.push(func(element))
+    });
+    return new_arr
+}
+
+function reduce(arr, func, stPoint) {
+    let current 
+    let i 
+    if (!!stPoint) {
+        current = stPoint
+        i = 0
+    } else {
+        current = arr[0]
+        i = 1
+    }
+    
+    for(i; i < arr.length; i++) {
+        current = func(arr[i], current)
+    }
+    return current
+}

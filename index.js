510ff5a41ca1a685be41e88320e6cd587044e211
mikeisfake <mikescooper@icloud.com>
2020-07-08 23:10:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map (src, callback) {
+  let result = []
+  for (let i=0; i< src.length; i++) {
+    let el = src[i]
+    result.push(callback(el))
+  }
+  return result
+}
+function reduce (src, callback, starting) {
+  let r = (!!starting) ? starting : src[0]
+ let i = (!!starting) ? 0 : 1
+
+ for (; i < src.length; i++) {
+   r = callback(src[i], r)
+ }
+
+ return r;
+}

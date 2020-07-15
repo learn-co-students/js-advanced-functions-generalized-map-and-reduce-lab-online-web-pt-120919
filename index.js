c8d7afbb4e352607dbe3562fda319d3e4130bc0d
MilanP5G <milan_parmar@hotmail.co.uk>
2020-07-15 11:01:45 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, callbackFunction) {
+  let newArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callbackFunction(array[i]))
+  }
+  return newArray
+}
+
+function reduce(array, callbackFunction, startValue) {
+  let total = !!startValue ? startValue : array[0]
+  let i = !!startValue ? 0 : 1
+  for (i; i < array.length; i++){
+    total = callbackFunction(array[i], total)
+  }
+  return total
+}

6a3fdf08b3d279dcae1cda4b09b708c3bb210a40
Sarah Condon <s.j.condon01@gmail.com>
2020-07-15 22:57:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(arr,fn) {
+    let mappedArr = []
+    arr.forEach(index => {
+        mappedArr.push(fn(index))
+    })
+    return mappedArr
+}
+
+function reduce(arr, fn, startingPt) {
+    let acc
+    let ind
+
+    if (!!startingPt) {
+        acc = startingPt
+        ind = 0
+    } else {
+        acc = fn(arr[0], arr[1])
+        ind = 2
+    }
+    for(ind; ind < arr.length; ind++) {
+        acc = fn(acc, arr[ind])
+    }
+    return acc
+}

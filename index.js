f397a020d6adab9e476034c57b28772259f2266e
Anthony <pardo.anthony@gmail.com>
2020-07-16 00:29:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions here
+function map(arr, foo) {
+  let tmp = [];
+  for (let i = 0; i < arr.length; i++) {
+    tmp.push(foo(arr[i]));
+  }
+  return tmp;
+}
+
+function reduce(arr, start = 0) {
+  let total = start;
+  for (let i = 0; i < arr.length; i++) {
+    tmp.push(foo(arr[i]));
+  }
+  return tmp;
+}
+
+function reduce(arr, foo, start){
+  let total;
+  if (!!start) {
+    total = start;
+  } else {
+    total = arr[0];
+  }
+  
+  for ( let i = !!start ? 0 : 1; i < arr.length; i++) {
+    total = foo(arr[i], total)
+  }
+  return total
+}

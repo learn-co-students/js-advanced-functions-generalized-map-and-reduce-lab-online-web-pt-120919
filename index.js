b3bcf5d8f5b6b89ca2b1a35c16bb862c9f11b694
Mely07 <damely.tineo07@gmail.com>
2020-07-16 00:29:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Add your functions here
+function map(sourceArray, callback) {
+    return sourceArray.map((value) => callback(value));
+}
+
+function reduce(sourceArray, callback, init){
+    if (init){
+        return sourceArray.reduce((accumulator, currentValue) => callback(accumulator, currentValue), init);
+    }
+    return sourceArray.reduce((accumulator, currentValue) => callback(accumulator, currentValue));
+}
+
+

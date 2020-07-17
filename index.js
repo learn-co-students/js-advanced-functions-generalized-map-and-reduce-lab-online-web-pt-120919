1ebb7dcec143fa719b6cea1e228753968361b476
Justin <justinms@outlook.com>
2020-07-17 12:22:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your functions here
+array = [1, 2, 3, 4]
+
+function map(array, fn){
+  let newArray = []
+  for (let i = 0; i < array.length; i++ ) {
+    newArray.push(fn(array[i]))
+  }
+  return newArray
+
+}
+
+function toNegative(arrayElement) {
+  return arrayElement * -1
+}
+
+function toOriginal(arrayElement) {
+  return arrayElement
+}
+
+function toDouble(arrayElement) {
+  return arrayElement * 2
+}
+
+function toSquared(arrayElement) {
+  return arrayElement ** 2
+}
+
+
+function reduce(array, fn, start){
+  // !! reduces a value to a boolean 
+
+  let value = (!!start) ? start : array[0]
+  let i = (!!start) ? 0 : 1
+
+  for ( i ; i < array.length; i++){
+    value = fn(array[i], value)
+  }
+
+  return value
+}
+
+
+
+
+// const newArray = map(array, toSquared)
+// console.log(newArray)

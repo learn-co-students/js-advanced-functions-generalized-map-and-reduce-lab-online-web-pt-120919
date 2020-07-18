3e3ab6de458794f9a498b46c5e0d89f819dc2a4d
asijaw <asija.w@yahoo.com>
2020-07-18 12:31:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(sourceArray, callback) {
+    let newArray = []
+  
+    for (let i = 0; i < sourceArray.length; i++) {
+      newArray.push(callback(sourceArray[i]))
+    }
+  
+    return newArray;
+}
+
+function reduce(sourceArray, callback, starting){
+    let total = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < sourceArray.length; i++) {
+      total = callback(sourceArray[i], total)
+    }
+  
+    return total;
+  }

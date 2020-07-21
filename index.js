4c0aa538f08e63fd71578593309d7ff520ff281a
Matthew <mattcorralejo1@gmail.com>
2020-07-21 13:41:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+
+function map(array, callback){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        let element = array[i]
+        newArray.push(callback(element))
+    }
+    return newArray
+}
+
+function reduce(array, callback, starter) {
+    let x = (!!starter) ? starter : array[0]
+    let i = (!!starter) ? 0 : 1
+    for(; i < array.length; i++){
+        x = callback(array[i], x)
+    }
+  return x
+}
+
+
+
+

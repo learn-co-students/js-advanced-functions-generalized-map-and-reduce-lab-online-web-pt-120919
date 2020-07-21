7d7d921c3bd8d067b770a34749fa8bbf9b26ef9d
SMakaiTakori <makait87@icloud.com>
2020-07-20 22:57:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, element){
+    let newArray = []
+
+    for (let i =0; i < array.length; i++){
+        let data = array[i]
+        newArray.push(element(data))
+    }
+    return newArray;
+}
+
+function reduce(array, element, start) {
+    let a = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for(; i < array.length; i++){
+        a = element(array[i], a)
+    }
+    return a;
+}

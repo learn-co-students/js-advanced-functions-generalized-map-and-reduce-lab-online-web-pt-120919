eb0bbf5f90a70a08cf6b0cfaf3ff9e54789646fa
gtblackmore <gtblackmore@gmail.com>
2020-07-22 13:08:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(sourceArray, callback){
+  let arr = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    arr.push(callback(sourceArray[i]))
+  }
+  return arr
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}

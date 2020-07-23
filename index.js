60469e02a92145a604dfca3d1df69581d7f8d6e4
thomas dubiel <github email address>
2020-07-23 01:07:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(array, callbackFunction) {
+  let new_array = []
+  for (let i = 0; i < array.length; i++) {
+    new_array.push(callbackFunction(array[i]));
+  }
+  return new_array;
+}
+
+function reduce(array, callbackFunction, thingToChange) {
+//decide if using integer or boolean to reduce
+//if there is a value in thingToChange, use it. else, start with the first
+//element in array
+
+  let starterInt = (!!thingToChange) ? thingToChange : array[0];
+  //if !!thingToChange returns truthy, i = 0. else, we start iterating at 1
+  let i = (!!thingToChange) ? 0 : 1;
+
+  for (i; i < array.length; i++) {
+    starterInt = callbackFunction(array[i], starterInt)
+  }
+  return starterInt;
+}
